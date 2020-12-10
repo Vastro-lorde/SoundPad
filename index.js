581c9debd1b085fa94a4c8edc46b297fc12584af
Seun Daniel Omatsola <omatsolaseund@gmail.com>
2020-12-10 10:59:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+document.querySelector(".chai").addEventListener("click", function(){
+var audio = new Audio("sounds/Chai.mp3");
+audio.play();
+});
+
+document.querySelector(".confuse").addEventListener("click", function(){
+var audio = new Audio("sounds/Confuse.mp3");
+audio.play();
+});
+
+document.querySelector(".scratch").addEventListener("click", function(){
+var audio = new Audio("sounds/Dj-Scratch.mp3");
+audio.play();
+});
+
+document.querySelector(".fart").addEventListener("click", function(){
+var audio = new Audio("sounds/Fart-Short.mp3");
+audio.play();
+});
+
+document.querySelector(".funke").addEventListener("click", function(){
+var audio = new Audio("sounds/Funke.mp3");
+audio.play();
+});
